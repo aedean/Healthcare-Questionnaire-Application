9911ac6c6f6aac66432472e410c5e78844fa279b
Alice Dean <deanomonkey@hotmail.co.uk>
2019-03-24 13:51:24 +0000

    Add base model files

---
+++ QuestionnaireApplication/database/migrations/2019_03_24_102238_create_question_answers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('question_answers', function (Blueprint $table) {
+            $table->increments('answerid');
+            $table->int('questionnaireid');
+            $table->int('questionid');
+            $table->int('languageid');
+            $table->string('answer');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('question_answers');
+    }
+}
