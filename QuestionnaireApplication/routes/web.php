5010e0bfb849e4fad68522152aae92dd9fdd22aa
Alice Dean <deanomonkey@hotmail.co.uk>
2019-03-24 22:38:36 +0000

    comment

--- QuestionnaireApplication/routes/web.php
+++ QuestionnaireApplication/routes/web.php
@@ -19,8 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+/* Users */
 Route::resource('address', 'UserAddressController');
-
 Route::resource('usertype', 'UserTypesController');
-
 Route::resource('user', 'UserController');
