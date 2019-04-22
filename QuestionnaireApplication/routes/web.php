a1ea8e67b8848792cb58a57ce515658d64962bea
Alice Dean <deanomonkey@hotmail.co.uk>
2019-04-22 20:24:52 +0100

    Add patients routing

--- QuestionnaireApplication/routes/web.php
+++ QuestionnaireApplication/routes/web.php
@@ -31,6 +31,8 @@ Route::resource('systemconfiguration', 'SystemConfigController');
 Route::resource('healthcareworkers', 'HealthcareWorkersController');
 Route::resource('usertypes', 'UserTypesController');
 
+/* Patients */
+Route::resource('patients', 'PatientsController');
 Route::prefix('patient')->group(function() {
     Route::get('/login', 'Auth\PatientLoginController@showLoginForm')->name('patient.login');
     Route::post('/login', 'Auth\PatientLoginController@login')->name('patient.login.submit');
