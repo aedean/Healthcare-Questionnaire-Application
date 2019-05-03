179a607d54979105e4ee4ae194233edfd73a86a2
Alice Dean <deanomonkey@hotmail.co.uk>
2019-05-03 12:47:38 +0100

    show healthcare contacts

--- QuestionnaireApplication/resources/views/healthcarecontacts/show.blade.php
+++ QuestionnaireApplication/resources/views/healthcarecontacts/show.blade.php
@@ -7,28 +7,12 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Healthcare Contacts</div>
                 <div class="panel-body">
-                    <table class="table">
-                        <thead>
-                            <th scope="col">Name</th>
-                            <th scope="col">Mobile</th>
-                            <th scope="col">Landline</th>
-                            <th scope="col">Company</th>
-                        </thead>
-                        <tbody>
-                            <?php $configCount = 1; ?>
-                            <?php foreach($healthcarecontacts as $healthcarecontact): ?>
-                                <tr>
-                                    <td><?php echo $healthcarecontact->name; ?></td>
-                                    <td><?php echo $healthcarecontact->mobile; ?></td>
-                                    <td><?php echo $healthcarecontact->landline; ?></td>
-                                    <td><?php echo $healthcarecontact->company; ?></td>
-                                </tr>
-                            <?php endforeach; ?>
-                        </tbody>
-                    </table>
+                    <div class="contacts-container">
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+<script src="{{ asset('js/getHealthcareContacts.js') }}"></script>
 @endsection
