a809d9ad17dbfec51f2bf9262ee4156f37c80e06
Alice Dean <deanomonkey@hotmail.co.uk>
2019-05-18 22:52:18 +0100

    Result show in questionnaire

--- QuestionnaireApplication/resources/views/result/index.blade.php
+++ QuestionnaireApplication/resources/views/result/index.blade.php
@@ -5,12 +5,17 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
+            <div class="panel-heading"><h3>Results</h3></div>
                 <div class="panel-body">
-                    
+                    <div class="results-container">
+                        <h3>You scored: </h3>
+
+                    </div>
+                    <div class="btn btn-secondary btn-lg questionnaire-btns results-next-btn questionnaire-continue">Continue</div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<script src="{{ asset('js/setQuestionnaires.js') }}"></script>
+<script src="{{ asset('js/getResults.js') }}"></script>
 @endsection
