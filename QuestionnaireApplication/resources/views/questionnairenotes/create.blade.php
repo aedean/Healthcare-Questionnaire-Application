16a2a675e3738c9d74a5346c1ed1b65ae8bfad6e
Alice Dean <deanomonkey@hotmail.co.uk>
2019-05-18 22:27:16 +0100

    Questionnaire notes

--- QuestionnaireApplication/resources/views/questionnairenotes/create.blade.php
+++ QuestionnaireApplication/resources/views/questionnairenotes/create.blade.php
@@ -5,33 +5,23 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
+            <div class="panel-heading"><h3>Notes</h3></div>
                 <div class="panel-body">
-                {!! Form::open(['action' => 'QuestionnaireNotesController@store', 'method' => 'POST', 'class' => 'form-horizontal', 'enctype' => 'multipart/form-data']) !!}
+                    <h4 class="col-md-4 control-label">Note</h4>
+                    <div class="form-group">
 
-                    <div class="form-group{{ $errors->has('note') ? ' has-error' : '' }}">
-                        <label for="note" class="col-md-4 control-label">Note</label>
-
-                        <div class="col-md-6">
-                            <input id="note" type="text" class="form-control" note="note" required autofocus>
-
-                            @if ($errors->has('note'))
-                                <span class="help-block">
-                                    <strong>{{ $errors->first('note') }}</strong>
-                                </span>
-                            @endif
+                        <div class="col-md-6 note">
+                            <input id="note" type="text" class="form-control" name="note" required autofocus>
                         </div>
                     </div>
-
-                <div class="form-group">
-                    <div class="col-md-8 col-md-offset-4">
-                    {!! Form::submit('Back to all questionnaires', ['class' => 'btn']) !!}
-                    </div>
-                </div>
-                {!! Form::close() !!}
+              
+                    <div class="col-md-8 col-md-offset-4 row-padding">
+                        <div class="btn questionnaires-back btn-lg btn-secondary questionnaire-continue">Back to all questionnaires</div>
+                    </div>       
                 </div>
             </div>
         </div>
     </div>
 </div>
-<script src="{{ asset('js/setQuestionnaires.js') }}"></script>
+<script src="{{ asset('js/setNote.js') }}"></script>
 @endsection
