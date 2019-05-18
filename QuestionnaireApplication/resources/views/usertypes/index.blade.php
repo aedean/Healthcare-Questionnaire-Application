9d0e0c88f5352854c94fac8e807a180fa144ff0d
Alice Dean <deanomonkey@hotmail.co.uk>
2019-05-18 22:23:40 +0100

    Update user types

--- QuestionnaireApplication/resources/views/usertypes/index.blade.php
+++ QuestionnaireApplication/resources/views/usertypes/index.blade.php
@@ -5,37 +5,37 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">UserTypes</div>
+                <div class="panel-heading"><h3>User Types</h3></div>
                 <div class="panel-body">
-                    <a href="<?php echo url('/') . '/systemconfiguration'; ?>" class="btn btn-default">Back</a>
-                    <table class="table table-bordered">
+                    <a href="<?php echo url('/') . '/systemconfiguration'; ?>" class="btn btn-default btn-secondary btn-lg">Back</a>
+                    <a href="<?php echo url('/') . '/usertypes/create'; ?>" class="btn btn-secondary btn-lg" role="button">Create</a>
+                    <table class="table">
                         <thead>
-                            <th scope="col">No.</th>
-                            <th scope="col">Id</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Edit</th>
-                            <th scope="col">Delete</th>
+                            <th scope="col"><h4>No.</h4></th>
+                            <th scope="col"><h4>Id</h4></th>
+                            <th scope="col"><h4>Name</h4></th>
+                            <th scope="col"><h4>Edit</h4></th>
+                            <th scope="col"><h4>Delete</h4></th>
                         </thead>
                         <tbody>
                             <?php $usertypeCount = 1; ?>
                             <?php foreach($usertypes as $usertype): ?>
                                 <tr>
-                                    <th scope="row"><?php echo $usertypeCount++; ?></th>
-                                    <td><?php echo $usertype->usertypeid; ?></td>
-                                    <td><?php echo $usertype->usertypename; ?></td>
-                                    <td><a href="<?php echo url('/') . '/usertypes/' . $usertype->usertypeid . '/edit'; ?>" class="btn btn-default">Edit<a></td>
-                                    <td class="table-dark">
+                                    <th scope="row"><h4><?php echo $usertypeCount++; ?></h4></th>
+                                    <td><h4><?php echo $usertype->usertypeid; ?></h4></td>
+                                    <td><h4><?php echo $usertype->usertypename; ?></h4></td>
+                                    <td><a href="<?php echo url('/') . '/usertypes/' . $usertype->usertypeid . '/edit'; ?>" class="btn btn-default btn-secondary btn-lg">Edit<a></td>
+                                    <td>
                                         {!! Form::open(['action' => ['UserTypesController@destroy', $usertype->usertypeid], 'method' => 'POST']) !!}
                                         {{ csrf_field() }}
                                             {!! Form::hidden('_method', 'DELETE') !!}
-                                            {!! Form::submit('Delete', ['class' => 'btn']) !!}
+                                            {!! Form::submit('Delete', ['class' => 'btn btn-secondary btn-lg']) !!}
                                         {!! Form::close() !!}
                                     </td>
                                 </tr>
                             <?php endforeach; ?>
                         </tbody>
                     </table>
-                    <a href="<?php echo url('/') . '/usertypes/create'; ?>" class="btn btn-success" role="button">Create</a>
                 </div>
             </div>
         </div>
