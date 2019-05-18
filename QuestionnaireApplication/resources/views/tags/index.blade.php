b51f22e452379bfa0681be8ba4c401fa3902a136
Alice Dean <deanomonkey@hotmail.co.uk>
2019-05-18 22:33:11 +0100

    Update system config

--- QuestionnaireApplication/resources/views/tags/index.blade.php
+++ QuestionnaireApplication/resources/views/tags/index.blade.php
@@ -5,20 +5,20 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Tags</div>
+                <div class="panel-heading"><h3>Tags</h3></div>
                 <div class="panel-body">
-                    <a href="<?php echo url('/') . '/systemconfiguration'; ?>" class="btn btn-default">Back</a>
-                    <table>
+                    <a href="<?php echo url('/') . '/systemconfiguration'; ?>" class="btn btn-default btn-secondary btn-lg">Back</a>
+                    <table class="table">
                         <thead>
-                            <th>No.</th>
-                            <th>Edit</th>
-                            <th>Delete</th>
+                            <th scope="col"><h4>No.</h4></th>
+                            <th scope="col"><h4>Edit</h4></th>
+                            <th scope="col"><h4>Delete</h4></th>
                         </thead>
                         <tbody>
                             <?php $configCount = 1; ?>
                             <?php foreach($tags as $tag): ?>
                                 <tr>
-                                    <td><?php echo $configCount++; ?></td>
+                                    <td scope="row"><h4><?php echo $configCount++; ?></h4></td>
                                     <td>
                                         {!! Form::open(['action' => ['TagsController@update', $tag->id], 'method' => 'POST', 'class' => 'form-horizontal']) !!}
                                         {{ csrf_field() }}
@@ -38,7 +38,7 @@
                                         <div class="form-group">
                                             <div class="col-md-8 col-md-offset-4">
                                             {{ Form::hidden('_method', 'PUT') }}
-                                            {!! Form::submit('Update', ['class' => 'btn']) !!}
+                                            {!! Form::submit('Update', ['class' => 'btn btn-default btn-secondary btn-lg']) !!}
                                             </div>
                                         </div>
                                     {!! Form::close() !!}
@@ -47,7 +47,7 @@
                                         {!! Form::open(['action' => ['TagsController@destroy', $tag->id], 'method' => 'POST']) !!}
                                         {{ csrf_field() }}
                                             {!! Form::hidden('_method', 'DELETE') !!}
-                                            {!! Form::submit('Delete', ['class' => 'btn']) !!}
+                                            {!! Form::submit('Delete', ['class' => 'btn btn-default btn-secondary btn-lg']) !!}
                                         {!! Form::close() !!}
                                     </td>
                                 </tr>
@@ -73,7 +73,7 @@
 
                     <div class="form-group">
                         <div class="col-md-8 col-md-offset-4">
-                        {!! Form::submit('Add Tag', ['class' => 'btn']) !!}
+                        {!! Form::submit('Add Tag', ['class' => 'btn btn-default btn-secondary btn-lg']) !!}
                         </div>
                     </div>
                     {!! Form::close() !!}
