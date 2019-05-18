1a6ce65e8d36b2d84c7ef45a7a0fc3813d3efd5c
Alice Dean <deanomonkey@hotmail.co.uk>
2019-05-18 22:26:33 +0100

    Update healthcare contacts

--- QuestionnaireApplication/resources/views/healthcarecontacts/show.blade.php
+++ QuestionnaireApplication/resources/views/healthcarecontacts/show.blade.php
@@ -5,14 +5,16 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Healthcare Contacts</div>
+                <div class="panel-heading"><h3>Healthcare Contacts</h3></div>
                 <div class="panel-body">
                     <div class="contacts-container">
                     </div>
+                    <div class="healthcontacts-btn questionnaire-continue btn-secondary btn-lg">Continue</div>
                 </div>
             </div>
         </div>
     </div>
 </div>
 <script src="{{ asset('js/getHealthcareContacts.js') }}"></script>
+<script src="{{ asset('js/getResults.js') }}"></script>
 @endsection
