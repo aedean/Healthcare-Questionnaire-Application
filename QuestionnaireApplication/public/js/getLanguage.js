32c68af50db14eba6f12103ec4ac43511c7ab5d0
Alice Dean <deanomonkey@hotmail.co.uk>
2019-05-18 22:54:27 +0100

    Start of translation js

---
+++ QuestionnaireApplication/public/js/getLanguage.js
@@ -0,0 +1,14 @@
+//https://translation.googleapis.com/language/translate/v2?key=AIzaSyCNS4m8AkKsSE2JkMHN3rJK5X_8nUh_-d0
+
+jQuery(document).ready(function() {
+
+    jQuery('.answer').val();
+
+    jQuery('.question').val();
+
+    // const url = ('https://translation.googleapis.com/language/translate/v2?key=AIzaSyCNS4m8AkKsSE2JkMHN3rJK5X_8nUh_-d0?target=eu?q=hello');
+    // jQuery.post(url, function(data) {  
+    //     console.log(data); 
+    // });
+
+});
