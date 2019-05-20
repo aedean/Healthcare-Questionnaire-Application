b5d47142eac674c15f75f9a337e74a0da2a5eb69
Alice Dean <deanomonkey@hotmail.co.uk>
2019-05-20 19:20:54 +0100

    Save notes and rework page

--- QuestionnaireApplication/public/js/notes.js
+++ QuestionnaireApplication/public/js/notes.js
@@ -1,8 +1,11 @@
-jQuery(document).on('click', '.save', function(event) { 
-    var resultid = localStorage.getItem('resultid');
-    var notes = jQuery('.notes').val();
-    var questionnaireid = localStorage.getItem("questionnaireid");
-    var notesname = "result" + resultid + "notes";
-    localStorage.setItem(notesname, notes);
-    document.location.href = "http://localhost/QuestionnaireApplication/QuestionnaireApplication/public/offline/questionnaires.html";
-});
+
+    alert('dd');
+    jQuery(document).on('click', '.save', function(event) { 
+        console.log(jQuery('.notes'));
+        // var resultid = localStorage.getItem('resultid');
+        // var notes = jQuery('.notes').val();
+        // var questionnaireid = localStorage.getItem("questionnaireid");
+        // var notesname = "result" + resultid + "notes";
+        // localStorage.setItem(notesname, notes);
+    // document.location.href = "http://localhost/QuestionnaireApplication/QuestionnaireApplication/public/offline/questionnaires.html";
+    });
